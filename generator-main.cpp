aadf755e8bbc76aaa44c53bf1287fb4e008178e1
chart21 <chart21@web.de>
2021-06-17 18:18:30 +0200

    added typeConversion

--- generator-main.cpp
+++ generator-main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 
 
+
 int main(int arg_count, char *args[])
 {
     if (arg_count > 1)
@@ -17,7 +18,7 @@ int main(int arg_count, char *args[])
         cout << "Username not supplied.. exiting the program \n";
     }
 
-
+    
 
     
     return 0;
---
+++ src/circuitProcessor/helperFunctions.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <random>
+#include <functional>
+#include <bitset>
+bool* generateRandomInput(size_t bitlength)
+{
+    auto gen = std::bind(std::uniform_int_distribution<>(0,1),std::default_random_engine());
+    bool *input = new bool[bitlength];
+
+    for (int i = 0; i < bitlength; i++)
+    {
+        input[i] = gen();
+    }
+    return input;
+}
+
+
+bool* converIntToBoolArr(int num, size_t bitlength)
+{
+bool *arr = new bool[bitlength];
+for (int i = 0; i < bitlength; i++)
+{
+  arr[i] = num & 1;
+  num /= 2;
+}
+return arr;
+}
+
+int convertBoolArrToInt(bool* arr, size_t bitlength)
+{
+    int num = 0;
+    int tmp;
+    for (int i = 0; i < bitlength; i++)
+    {
+    tmp = arr[i];
+    num |= tmp << (bitlength - i - 1);    
+    }
+    return num;
+}
