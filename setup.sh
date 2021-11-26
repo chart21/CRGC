39156b3e3999e60123e15964f9699ce733ce6570
Fan Fei <ffclaire1224@gmail.com>
2021-11-26 13:48:39 +0000

    fix directory error again

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ echo "--- Intall Dependencies for Linux Ubuntu ---"
 
 apt-get install -y software-properties-common
 apt-get update
-apt-get install -y cmake git build-essential libssl-dev libgmp-dev python wget
+apt-get install -y cmake git build-essential libssl-dev libgmp-dev python wget xxd
 apt-get install -y libboost-dev
 apt-get install -y libboost-{chrono,log,program-options,date-time,thread,system,filesystem,regex,test}-dev
 
@@ -14,7 +14,7 @@ wget https://raw.githubusercontent.com/emp-toolkit/emp-readme/master/scripts/ins
 python install.py --deps --tool --ot --sh2pc
 
 echo "--- Fetch TurboPFor ---"
-cd CRGC-dep/Reusable-Garbled-Circuit-Generator-CPP
+cd Reusable-Garbled-Circuit-Generator-CPP
 git submodule update --init --recursive
 
 echo "--- Build CRGC ---"
