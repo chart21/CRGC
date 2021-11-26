d2284c1163962dc269325f058c2c27601295eba0
Fan Fei <ffclaire1224@gmail.com>
2021-11-26 13:40:09 +0000

    update dependency files, add wget package

--- setup.sh
+++ setup.sh
@@ -2,24 +2,12 @@
 
 cd ..
 echo "--- Intall Dependencies for Linux Ubuntu ---"
-	CC=`lsb_release -rs | cut -c 1-2`
-	VER=`expr $CC + 0`
-	if [[ $VER -gt 15 ]]; then
-		apt-get install -y software-properties-common
-		apt-get update
-		apt-get install -y cmake git build-essential libssl-dev libgmp-dev python 
-		apt-get install -y libboost-dev
-		apt-get install -y libboost-{chrono,log,program-options,date-time,thread,system,filesystem,regex,test}-dev
-	else
-		apt-get install -y software-properties-common
-		add-apt-repository -y ppa:george-edison55/cmake-3.x
-		add-apt-repository -y ppa:kojoley/boost
-		apt-get update
-		apt-get install -y cmake git build-essential libssl-dev libgmp-dev python 
-		apt-get install -y libboost-all-dev
-#		sudo apt-get install -y libboost1.58-dev
-#		sudo apt-get install -y libboost-{chrono,log,program-options,date-time,thread,system,filesystem,regex,test}1.58-dev
-	fi
+
+apt-get install -y software-properties-common
+apt-get update
+apt-get install -y cmake git build-essential libssl-dev libgmp-dev python wget
+apt-get install -y libboost-dev
+apt-get install -y libboost-{chrono,log,program-options,date-time,thread,system,filesystem,regex,test}-dev
 
 echo "--- Fetch EMP-lib ---"
 wget https://raw.githubusercontent.com/emp-toolkit/emp-readme/master/scripts/install.py
